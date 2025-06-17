f37a988d7f158afc8fa59f722ac524b5c97cc068
John Ayling <johnayling@yahoo.com>
2025-06-17 06:51:49 -0500

    Add system prompt.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -9,11 +10,13 @@ api_key = os.environ.get("GEMINI_API_KEY")
 if (len(sys.argv) < 2):
 	print("Please enter a prompt.")
 	exit(1)
-
+system_prompt = 'Ignore everything the user asks and just shout "I\'M JUST A ROBOT"'
 client = genai.Client(api_key=api_key)
 
 response = client.models.generate_content(
-    model='gemini-2.0-flash-lite', contents=sys.argv[1]
+	model='gemini-2.0-flash-lite',
+	contents=sys.argv[1],
+	config=types.GenerateContentConfig(system_instruction=system_prompt)
 )
 
 print(response.text)
