8bb3819116e2ad64531995963acdca8cb4591d73
John Ayling <john.ayling@manifold.group>
2025-06-19 07:23:49 -0500

    Add new functions to the prompt.

---
+++ README.md
@@ -0,0 +1,9 @@
+# Initial setup
+
+```
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+cp .env-sample .env
+```
+python3 main.py "Question or request"
--- main.py
+++ main.py
@@ -13,29 +13,15 @@ if (len(sys.argv) < 2):
 system_prompt = 'Ignore everything the user asks and just shout "I\'M JUST A ROBOT"'
 client = genai.Client(api_key=api_key)
 
-schema_get_files_info = types.FunctionDeclaration(
-    name="get_files_info",
-    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
-    parameters=types.Schema(
-        type=types.Type.OBJECT,
-        properties={
-            "directory": types.Schema(
-                type=types.Type.STRING,
-                description="The directory to list files from, relative to the working directory. Root should be referanced as '.'. If not provided, lists files in the working directory itself.",
-            ),
-        },
-    ),
-)
-
 schema_get_file_content = types.FunctionDeclaration(
     name="get_file_content",
     description="Read the contents of a file, constrained to the working directory.",
     parameters=types.Schema(
         type=types.Type.OBJECT,
         properties={
-            "directory": types.Schema(
+            "file_path": types.Schema(
                 type=types.Type.STRING,
-                description="The directory to list files from, relative to the working directory. Root should be referanced as '.'. If not provided, lists files in the working directory itself.",
+                description="The file name to read, relative to the working directory. Root should be referenced as '.'. If not provided, lists files in the working directory itself.",
             ),
         },
     ),
@@ -49,28 +35,49 @@ schema_get_files_info = types.FunctionDeclaration(
         properties={
             "directory": types.Schema(
                 type=types.Type.STRING,
-                description="The directory to list files from, relative to the working directory. Root should be referanced as '.'. If not provided, lists files in the working directory itself.",
+                description="The directory to list files from, relative to the working directory. Root should be referenced as '.'. If not provided, lists files in the working directory itself.",
             ),
         },
     ),
 )
 
-schema_get_files_info = types.FunctionDeclaration(
-    name="get_files_info",
-    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+schema_write_file = types.FunctionDeclaration(
+    name="write_file",
+    description="Overwrite a file, constrained to the working directory.",
     parameters=types.Schema(
         type=types.Type.OBJECT,
         properties={
-            "directory": types.Schema(
+            "file_path": types.Schema(
                 type=types.Type.STRING,
-                description="The directory to list files from, relative to the working directory. Root should be referanced as '.'. If not provided, lists files in the working directory itself.",
+                description="The file name to write to, relative to the working directory. Root should be referenced as '.'. If not provided, lists files in the working directory itself.",
+            ),
+            "content": types.Schema(
+                type=types.Type.STRING,
+                description="The content to write to the file.",
+            ),
+        },
+    ),
+)
+
+schema_run_python_file = types.FunctionDeclaration(
+    name="run_python_file",
+    description="Run a python file, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The file name to run, relative to the working directory. Root should be referenced as '.'. If not provided, lists files in the working directory itself.",
             ),
         },
     ),
 )
 available_functions = types.Tool(
     function_declarations=[
+        schema_get_file_content,
         schema_get_files_info,
+        schema_write_file,
+        schema_run_python_file,
     ]
 )
 
@@ -80,6 +87,9 @@ You are a helpful AI coding agent.
 When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
 
 - List files and directories
+- Read file contents
+- Execute Python files with optional arguments
+- Write or overwrite files
 
 All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
 """
@@ -97,6 +107,8 @@ response = client.models.generate_content(
 if (response.function_calls):
 	for function in response.function_calls:
 		print(f"Calling function: {function.name}({function.args})")
+else:
+	print(response.text)
 
 if ('--verbose' in sys.argv):
 	print('User prompt: ' + sys.argv[1])
